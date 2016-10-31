9874dea6878a1e9888a1e14402d8d16338f749c4
Nico Watine <nico@studioncreations.com>
2016-10-31 13:29:05 -0500

    Reset layout/default and cleared index page
    Default layout had been updated in zurb-f6-boilerplate so had to reflect that in the project
    
    Removed the og-entries that were previously there by default. Later on they will be added but for now cramped things.
    
    Updated jquery version
    
    Updated .htaccess to remove the x-ua entry since that is now addressed within the actual layout html
    
    Merge and close

--- src/pages/index.php
+++ src/pages/index.php
@@ -8,10 +8,3 @@ og-image:
 body-id: index
 body-class:
 ---
-<h1>Studio N Creations</h1>
-<h2>A Visual Type Scale</h2>
-<h3>Visual Branding</h3>
-<h4>A Visual Type Scale</h4>
-<h5>A Visual Type Scale</h5>
-<h6>A Visual Type Scale</h6>
-<p>I'm team leader of the Design Department and in charge of the Coding Division. I do Administrative work, too - I'm also the head of Human Resources! That tends to work out well whenever I get unhappy with working conditions. I'm a web developer and am passionate about what I do. I love working with people who are excited about their new web project.</p>
