680d0a440898c8b52f1231fe427bd9a789f704b2
Nico Watine <nico@studioncreations.com>
2016-10-31 13:07:00 -0500

    Able to remove font-family from h2,p,h5,h6
    Wasn’t sure if necessary since they have different font weights
    
    Turns out we don’t have to specificy the font-family twice

--- src/pages/index.php
+++ src/pages/index.php
@@ -8,3 +8,10 @@ og-image:
 body-id: index
 body-class:
 ---
+<h1>A Visual Type Scale</h1>
+<h2>A Visual Type Scale</h2>
+<h3>A Visual Type Scale</h3>
+<h4>A Visual Type Scale</h4>
+<h5>A Visual Type Scale</h5>
+<h6>A Visual Type Scale</h6>
+<p>A Visual Type Scale</p>
