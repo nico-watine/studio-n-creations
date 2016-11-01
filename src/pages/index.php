a3dc5d134805e8f321c4bdca5e8bc9cfe40a22e1
Nico Watine <nico@studioncreations.com>
2016-11-01 12:32:44 -0500

    Adjusted .absolute-wrapper to better fit space

--- src/pages/index.php
+++ src/pages/index.php
@@ -9,12 +9,14 @@ body-id: index
 body-class: index
 ---
 <header>
-	<div class="row">
-		<div class="medium-6 large-5 columns">
-			<h1>Studio N Creations</h1>
-		</div>
-		<div class="medium-6 large-7 columns">
-			<h2 class="index-header-tagline">Web Design <strong>|</strong> Graphic Branding <span class="inline-block"><strong>|</strong> Site Maintenance</span></h2>
+	<div class="absolute-wrapper">
+		<div class="row">
+			<div class="medium-6 large-5 columns">
+				<h1>Studio N Creations</h1>
+			</div>
+			<div class="medium-6 large-7 columns">
+				<h2 class="index-header-tagline">Web Design <strong>|</strong> Graphic Branding <span class="inline-block"><strong>|</strong> Site Maintenance</span></h2>
+			</div>
 		</div>
 	</div>
 	<div class="gradient"></div>
