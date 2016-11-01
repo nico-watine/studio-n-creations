76673db2d49b88c0484683e7e2f6b9f354a171c4
Nico Watine <nico@studioncreations.com>
2016-11-01 12:29:16 -0500

    <header> with h1 and h2 and bg set
    Currently the sass is pretty messy (as in block placement dependent). I’m not sure to what extent I’ll be able to efficiently (time-wise) separate all the elements into chunk for styling.
    <header> bg image (isometric-ipads) needs to be optimized, probably turned into jpg

--- src/pages/index.php
+++ src/pages/index.php
@@ -6,5 +6,16 @@ og-title:
 og-description:
 og-image:
 body-id: index
-body-class:
+body-class: index
 ---
+<header>
+	<div class="row">
+		<div class="medium-6 large-5 columns">
+			<h1>Studio N Creations</h1>
+		</div>
+		<div class="medium-6 large-7 columns">
+			<h2 class="index-header-tagline">Web Design <strong>|</strong> Graphic Branding <span class="inline-block"><strong>|</strong> Site Maintenance</span></h2>
+		</div>
+	</div>
+	<div class="gradient"></div>
+</header>
