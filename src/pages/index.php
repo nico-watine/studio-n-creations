cca2b1db19fbcdac0b7e1ea39739ff5700d17a7c
Nico Watine <nico@studioncreations.com>
2016-11-05 14:59:34 -0500

    Tweak to title of dmesa image

--- src/pages/index.php
+++ src/pages/index.php
@@ -91,7 +91,7 @@ body-class: index
 					<img class="show-for-large" src="/img/index/graphic-branding/dmesa-home-logo.png" alt="d'Mesa Home logo" title="The d'Mesa Home website logo, designed by Studio N Creations">
 				</div>
 				<div class="medium-push-4 medium-3 columns hide-for-large text-center">
-					<img  src="/img/index/graphic-branding/dmesa-home-logo.png" alt="d'Mesa Home logo" title="The d'Mesa Home website logo, designed by Studio N Creations">
+					<img  src="/img/index/graphic-branding/dmesa-home-logo.png" alt="d'Mesa Home logo" title="The d'Mesa Home company logo, designed by Studio N Creations">
 				</div>
 				<div class="medium-5 large-8 columns">
 					<img src="/img/index/graphic-branding/jardin-de-france-logo.png" alt="Jardin de France TX logo" title="The Jardin de France company logo, shown here on a price tag">
