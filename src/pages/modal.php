e40ce5418e3372244cd330c95dc6d170a287ee7e
Nico Watine <nico@studioncreations.com>
2016-11-06 14:37:30 -0600

    All inline-styles converted to sass, px to em/rem
    This means divs were assigned classes
    
    This specific modal is for ESP, so those custom styles regarding the three images means they need to be specified via a main modal ID
    
    Do that next

--- src/pages/modal.php
+++ src/pages/modal.php
@@ -10,29 +10,29 @@ body-class: modal-page
 ---
 <p><a data-open="essential-sound-production-modal">Click me for a modal</a></p>
 
-<div class="reveal row" id="essential-sound-production-modal" style="background: none; padding-bottom: 15px;" data-reveal>
-	<div class="medium-10 medium-push-1 columns end recent-projects-modal" style="padding: 0;">
-		<div style="background: #fff; padding: 25px; border-radius: 4px 4px 0 0">
-			<h3 style="margin-bottom: 0;">Essential Sound Production</h3>
-			<h4 style="margin-bottom: 0;">Project Name</h4>
+<div class="reveal row recent-projects-modal" id="essential-sound-production-modal" data-reveal>
+	<div class="medium-10 medium-push-1 columns end modal-container">
+		<div class="modal-header">
+			<h3>Essential Sound Production</h3>
+			<h4>Project Name</h4>
 		</div>
-		<div style="padding: 25px 16px;" class="row collapse">
+		<div class="row collapse modal-prose">
 			<div class="large-7 columns">
 				<p>We’re currently in the middle of creating detailed project profile pages. In the meantime, why don’t you visit this project’s website?</p>
 			</div>
 			<div class="large-5 columns text-center">
-				<a class="button custom" style="border-radius: 2px; box-shadow: -4px 4px 0px 0px rgba(141,149,158,0.50); background: #566372; font-size: 16px;" href="#">www.EssentialSoundProduction.com</a>
+				<a class="button modal-site-link" href="#">www.EssentialSoundProduction.com</a>
 			</div>
 		</div>
-		<div class="row image-row collapse" style="border-radius: 0 0 4px 4px; overflow: hidden; position: relative;">
-			<div class="medium-5 columns" style="position: absolute; background: #fff; height: 100%;">
+		<div class="row collapse modal-images">
+			<div class="medium-5 columns image-1">
 				<img src="/img/index/recent-projects/modals/essential-sound-production/essential-sound-production-laptop-splash.png" alt="">
 			</div>
-			<div class="medium-3 medium-push-5 columns">
+			<div class="medium-3 medium-push-5 columns image-2">
 				<img src="/img/index/recent-projects/modals/essential-sound-production/essential-sound-production-dj-set.png" alt="">
 			</div>
-			<div class="medium-4 columns">
-				<img style="position: absolute; height: 100%;" src="/img/index/recent-projects/modals/essential-sound-production/essential-sound-production-about-tablet.jpg" alt="">
+			<div class="medium-4 columns image-3">
+				<img src="/img/index/recent-projects/modals/essential-sound-production/essential-sound-production-about-tablet.jpg" alt="">
 			</div>
 		</div>
 		<button class="close-button" data-close aria-label="Close modal" type="button">
