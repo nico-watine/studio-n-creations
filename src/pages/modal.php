eef599c0c6cfc473dc3b5ac685369613949bff33
Nico Watine <nico@studioncreations.com>
2016-11-06 12:18:39 -0600

    New modal page, inline styles (ESP example)
    Working on the modal for ESP based on the Sketch design, figuring out how to work width :: based on built-in classes, such as large, medium etc?
    Currently almost all styles are inline in the html
    BUT
    did create the _modal.scss page since eventually everything will be in there (and then imported from the _index.scss)

---
+++ src/pages/modal.php
@@ -0,0 +1,32 @@
+---
+url: /
+title:
+description:
+og-title:
+og-description:
+og-image:
+body-id: modal-page
+body-class: modal-page
+---
+<p><a data-open="essential-sound-production-modal">Click me for a modal</a></p>
+
+<div class="large reveal recent-projects-modal" id="essential-sound-production-modal" data-reveal>
+	<div style="background: #fff; padding: 25px;" class="row">
+		<h3 style="margin-bottom: 0;">Essential Sound Production</h3>
+		<h4 style="margin-bottom: 0;">Project Name</h4>
+	</div>
+	<div style="padding: 25px 16px;" class="row">
+		<div class="medium-7 columns">
+			<p>We’re currently in the middle of creating detailed project profile pages. In the meantime, why don’t you visit this project’s website?</p>
+		</div>
+		<div class="medium-5 columns text-center">
+			<a class="button custom" style="border-radius: 2px; box-shadow: -4px 4px 0px 0px rgba(141,149,158,0.50); background: #566372; font-size: 16px;" href="#">www.EssentialSoundProduction.com</a>
+		</div>
+	</div>
+	<div class="row">
+		
+	</div>
+	<button class="close-button" data-close aria-label="Close modal" type="button">
+		<span aria-hidden="true">&times;</span>
+	</button>
+</div>
