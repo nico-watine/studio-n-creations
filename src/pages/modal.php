fa26767618610f027b2fcaded4d094dc4627a5dd
Nico Watine <nico@studioncreations.com>
2016-11-06 13:34:11 -0600

    Nested columns in nested rows for the modals
    Coming together
    
    Still all inline-styles

--- src/pages/modal.php
+++ src/pages/modal.php
@@ -11,23 +11,29 @@ body-class: modal-page
 <p><a data-open="essential-sound-production-modal">Click me for a modal</a></p>
 
 <div class="reveal row" id="essential-sound-production-modal" style="background: none; padding-bottom: 15px;" data-reveal>
-	<div class="medium-10 medium-push-1 columns end recent-projects-modal">
-		<div style="background: #fff; padding: 25px; border-radius: 4px 4px 0 0" class="row">
+	<div class="medium-10 medium-push-1 columns end recent-projects-modal" style="padding: 0;">
+		<div style="background: #fff; padding: 25px; border-radius: 4px 4px 0 0">
 			<h3 style="margin-bottom: 0;">Essential Sound Production</h3>
 			<h4 style="margin-bottom: 0;">Project Name</h4>
 		</div>
-		<div style="padding: 25px 16px;" class="row">
-			<div class="medium-7 columns">
+		<div style="padding: 25px 16px;" class="row collapse">
+			<div class="large-7 columns">
 				<p>We’re currently in the middle of creating detailed project profile pages. In the meantime, why don’t you visit this project’s website?</p>
 			</div>
-			<div class="medium-5 columns text-center">
+			<div class="large-5 columns text-center">
 				<a class="button custom" style="border-radius: 2px; box-shadow: -4px 4px 0px 0px rgba(141,149,158,0.50); background: #566372; font-size: 16px;" href="#">www.EssentialSoundProduction.com</a>
 			</div>
 		</div>
-		<div class="row image-row" style="border-radius: 0 0 4px 4px; overflow: hidden;"">
-			<img src="/img/index/recent-projects/modals/essential-sound-production/essential-sound-production-laptop-splash.png" alt="">
-			<img src="/img/index/recent-projects/modals/essential-sound-production/essential-sound-production-dj-set.png" alt="">
-			<img src="/img/index/recent-projects/modals/essential-sound-production/essential-sound-production-about-tablet.jpg" alt="">
+		<div class="row image-row collapse" style="border-radius: 0 0 4px 4px; overflow: hidden;">
+			<div class="medium-5 columns">
+				<img src="/img/index/recent-projects/modals/essential-sound-production/essential-sound-production-laptop-splash.png" alt="">
+			</div>
+			<div class="medium-3 columns">
+				<img src="/img/index/recent-projects/modals/essential-sound-production/essential-sound-production-dj-set.png" alt="">
+			</div>
+			<div class="medium-4 columns">
+				<img src="/img/index/recent-projects/modals/essential-sound-production/essential-sound-production-about-tablet.jpg" alt="">
+			</div>
 		</div>
 		<button class="close-button" data-close aria-label="Close modal" type="button">
 			<span aria-hidden="true">&times;</span>
