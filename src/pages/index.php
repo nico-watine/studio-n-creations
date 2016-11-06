949b2f2f23ddbc31ee017a9102924100713cec2f
Nico Watine <nico@studioncreations.com>
2016-11-06 16:09:38 -0600

    ESP modal copied to index and linked up
    Previously all recent-project images linked direct to websites
    
    NOW
    ESP links to its modal, and modal has live link to website

--- src/pages/index.php
+++ src/pages/index.php
@@ -47,7 +47,7 @@ body-class: index
 		</div>
 		<div class="medium-6 columns">
 			<div class="project">
-				<a href="http://essentialsoundproduction.com/" target="_blank" title="Studio N Creations project link to EssentialSoundProduction.com"><img src="/img/index/recent-projects/essential-sound-production.png" alt="Essential Sound Production website mockup"></a>
+				<a data-open="essential-sound-production-modal" title="Essential Sound Production"><img src="/img/index/recent-projects/essential-sound-production.png" alt="Essential Sound Production website mockup"></a>
 			</div>
 		</div>
 	</div>
@@ -112,3 +112,34 @@ body-class: index
 		</div>
 	</div>
 </section>
+
+<div class="reveal row recent-projects-modal" id="essential-sound-production-modal" data-reveal>
+	<div class="medium-10 medium-push-1 columns end modal-container">
+		<div class="modal-header">
+			<h3>Essential Sound Production</h3>
+			<h4>Project Name</h4>
+		</div>
+		<div class="row collapse modal-prose">
+			<div class="large-7 columns">
+				<p>We’re currently in the middle of creating detailed project profile pages. In the meantime, why don’t you visit this project’s website?</p>
+			</div>
+			<div class="large-5 columns text-center">
+				<a class="button modal-site-link" href="http://essentialsoundproduction.com" target="_blank">www.EssentialSoundProduction.com</a>
+			</div>
+		</div>
+		<div class="row collapse modal-images">
+			<div class="small-8 medium-5 columns image-1">
+				<img src="/img/index/recent-projects/modals/essential-sound-production/essential-sound-production-laptop-splash.png" alt="Essential Sound Production landing-screen laptop mockup">
+			</div>
+			<div class="small-4 medium-3 medium-push-5 columns image-2">
+				<img src="/img/index/recent-projects/modals/essential-sound-production/essential-sound-production-dj-scene.png" alt="Essential Sound Production iPad DJ scene mockup">
+			</div>
+			<div class="small-12 medium-4 columns image-3">
+				<img src="/img/index/recent-projects/modals/essential-sound-production/essential-sound-production-about-tablet.jpg" alt="Essential Sound Production about-section tablet mockup">
+			</div>
+		</div>
+		<button class="close-button" data-close aria-label="Close modal" type="button">
+			<span aria-hidden="true">&times;</span>
+		</button>
+	</div>
+</div>
