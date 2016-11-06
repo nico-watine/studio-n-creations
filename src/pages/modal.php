ffdb5eff684fb983c2bd907e09f7c3f606879f42
Nico Watine <nico@studioncreations.com>
2016-11-06 12:33:01 -0600

    Converted modal to inner-container row
    So the .reveal div is just a wrapper (with no background) and everything is nested in the .recent-projects-modal

--- src/pages/modal.php
+++ src/pages/modal.php
@@ -10,23 +10,27 @@ body-class: modal-page
 ---
 <p><a data-open="essential-sound-production-modal">Click me for a modal</a></p>
 
-<div class="large reveal recent-projects-modal" id="essential-sound-production-modal" data-reveal>
-	<div style="background: #fff; padding: 25px;" class="row">
-		<h3 style="margin-bottom: 0;">Essential Sound Production</h3>
-		<h4 style="margin-bottom: 0;">Project Name</h4>
-	</div>
-	<div style="padding: 25px 16px;" class="row">
-		<div class="medium-7 columns">
-			<p>We’re currently in the middle of creating detailed project profile pages. In the meantime, why don’t you visit this project’s website?</p>
+<div class="reveal row" id="essential-sound-production-modal" style="background: none; padding-bottom: 15px;" data-reveal>
+	<div class="medium-10 medium-push-1 columns end recent-projects-modal">
+		<div style="background: #fff; padding: 25px; border-radius: 4px 4px 0 0" class="row">
+			<h3 style="margin-bottom: 0;">Essential Sound Production</h3>
+			<h4 style="margin-bottom: 0;">Project Name</h4>
 		</div>
-		<div class="medium-5 columns text-center">
-			<a class="button custom" style="border-radius: 2px; box-shadow: -4px 4px 0px 0px rgba(141,149,158,0.50); background: #566372; font-size: 16px;" href="#">www.EssentialSoundProduction.com</a>
+		<div style="padding: 25px 16px;" class="row">
+			<div class="medium-7 columns">
+				<p>We’re currently in the middle of creating detailed project profile pages. In the meantime, why don’t you visit this project’s website?</p>
+			</div>
+			<div class="medium-5 columns text-center">
+				<a class="button custom" style="border-radius: 2px; box-shadow: -4px 4px 0px 0px rgba(141,149,158,0.50); background: #566372; font-size: 16px;" href="#">www.EssentialSoundProduction.com</a>
+			</div>
 		</div>
+		<div class="row image-row" style="border-radius: 0 0 4px 4px; overflow: hidden;"">
+			<img src="/img/index/recent-projects/modals/essential-sound-production/essential-sound-production-laptop-splash.png" alt="">
+			<img src="/img/index/recent-projects/modals/essential-sound-production/essential-sound-production-dj-set.png" alt="">
+			<img src="/img/index/recent-projects/modals/essential-sound-production/essential-sound-production-about-tablet.jpg" alt="">
+		</div>
+		<button class="close-button" data-close aria-label="Close modal" type="button">
+			<span aria-hidden="true">&times;</span>
+		</button>
 	</div>
-	<div class="row">
-		
-	</div>
-	<button class="close-button" data-close aria-label="Close modal" type="button">
-		<span aria-hidden="true">&times;</span>
-	</button>
 </div>
