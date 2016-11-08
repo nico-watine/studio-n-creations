591731ad0aeb6a6409bb69c42ae7f69054eadc1a
Nico Watine <nico@studioncreations.com>
2016-11-08 11:11:31 -0600

    Copy over modal page content to index
    Merge and close

--- src/pages/index.php
+++ src/pages/index.php
@@ -66,7 +66,7 @@ body-class: index
 	<div class="row">
 		<div class="medium-6 columns">
 			<div class="project">
-				<a href="http://starhillfarms.com/" target="_blank" title="Studio N Creations project link to StarhillFarms.com"><img src="/img/index/recent-projects/starhill-farms.png" alt="Starhill Farms website mockup"></a>
+				<a data-open="starhill-farms-modal" title="StarHill Farms"><img src="/img/index/recent-projects/starhill-farms.png" alt="Starhill Farms website mockup"></a>
 			</div>
 		</div>
 		<div class="medium-6 columns">
@@ -190,6 +190,33 @@ body-class: index
 			</div>
 			<div class="medium-6 columns">
 				<img src="/img/index/recent-projects/modals/jardin-de-france-tx/jardin-de-france-tx-about.png" alt="Jardin de France TX About Section mockup">
+			</div>
+		</div>
+		<button class="close-button" data-close aria-label="Close modal" type="button">
+			<span aria-hidden="true">&times;</span>
+		</button>
+	</div>
+</div>
+<div class="reveal row recent-projects-modal" id="starhill-farms-modal" data-reveal>
+	<div class="medium-10 medium-push-1 columns end modal-container">
+		<div class="modal-header">
+			<h3>StarHill Farms</h3>
+			<h4>Project Name</h4>
+		</div>
+		<div class="row collapse modal-prose">
+			<div class="large-7 columns">
+				<p>We’re currently in the middle of creating detailed project profile pages. In the meantime, why don’t you visit this project’s website?</p>
+			</div>
+			<div class="large-5 columns large-text-center">
+				<a class="button modal-site-link" href="http://starhillfarms.com/" target="_blank">www.StarHillFarms.com</a>
+			</div>
+		</div>
+		<div class="row collapse modal-images">
+			<div class="medium-6 columns">
+				<img src="/img/index/recent-projects/modals/starhill-farms/starhill-farms-reservations.png" alt="StarHill Farms Reservations Page Mockup">
+			</div>
+			<div class="medium-6 columns">
+				<img src="/img/index/recent-projects/modals/starhill-farms/starhill-farms-index.png" alt="Artist StarHill Farms Landing Screen Mockup">
 			</div>
 		</div>
 		<button class="close-button" data-close aria-label="Close modal" type="button">
