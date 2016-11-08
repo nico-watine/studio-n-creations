190f453db99ec660839f1f77ba1bd46d3d4b5b80
Nico Watine <nico@studioncreations.com>
2016-11-08 11:09:54 -0600

    Inserted shf images and modal
    Images still to be optimized

--- src/pages/modal.php
+++ src/pages/modal.php
@@ -8,12 +8,12 @@ og-image:
 body-id: modal-page
 body-class: modal-page
 ---
-<p><a data-open="lisa-esposito-modal">Click me for a modal</a></p>
+<p><a data-open="starhill-farms-modal">Click me for a modal</a></p>
 
-<div class="reveal row recent-projects-modal" id="lisa-esposito-modal" data-reveal>
+<div class="reveal row recent-projects-modal" id="starhill-farms-modal" data-reveal>
 	<div class="medium-10 medium-push-1 columns end modal-container">
 		<div class="modal-header">
-			<h3>Artist Lisa Esposito</h3>
+			<h3>StarHill Farms</h3>
 			<h4>Project Name</h4>
 		</div>
 		<div class="row collapse modal-prose">
@@ -21,15 +21,15 @@ body-class: modal-page
 				<p>We’re currently in the middle of creating detailed project profile pages. In the meantime, why don’t you visit this project’s website?</p>
 			</div>
 			<div class="large-5 columns large-text-center">
-				<a class="button modal-site-link" href="http://www.lisaesposito.com" target="_blank">www.LisaEsposito.com</a>
+				<a class="button modal-site-link" href="http://starhillfarms.com/" target="_blank">www.StarHillFarms.com</a>
 			</div>
 		</div>
 		<div class="row collapse modal-images">
 			<div class="medium-6 columns">
-				<img src="/img/index/recent-projects/modals/lisa-esposito/lisa-esposito-index-landing.jpg" alt="Artist Lisa Esposito Welcome Screen mockup">
+				<img src="/img/index/recent-projects/modals/starhill-farms/starhill-farms-reservations.png" alt="StarHill Farms Reservations Page Mockup">
 			</div>
 			<div class="medium-6 columns">
-				<img src="/img/index/recent-projects/modals/lisa-esposito/lisa-esposito-upcoming-events.png" alt="Artist Lisa Esposito Upcoming Events mockup">
+				<img src="/img/index/recent-projects/modals/starhill-farms/starhill-farms-index.png" alt="Artist StarHill Farms Landing Screen Mockup">
 			</div>
 		</div>
 		<button class="close-button" data-close aria-label="Close modal" type="button">
