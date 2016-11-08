4a4b75696173789a416c6fbc659a079781418690
Nico Watine <nico@studioncreations.com>
2016-11-08 11:22:23 -0600

    Copy /modal content to index
    Merge and close now

--- src/pages/index.php
+++ src/pages/index.php
@@ -42,7 +42,7 @@ body-class: index
 	<div class="row">
 		<div class="medium-6 columns">
 			<div class="project">
-				<a href="http://houstonpals.com/" target="_blank" title="Studio N Creations project link to HoustonPALS.com"><img src="/img/index/recent-projects/houston-pals.png" alt="Houston PALS website mockup"></a>
+				<a data-open="houston-pals-modal"><img src="/img/index/recent-projects/houston-pals.png" alt="Houston PALS website mockup"></a>
 			</div>
 		</div>
 		<div class="medium-6 columns">
@@ -217,6 +217,33 @@ body-class: index
 			</div>
 			<div class="medium-6 columns">
 				<img src="/img/index/recent-projects/modals/starhill-farms/starhill-farms-index.png" alt="Artist StarHill Farms Landing Screen Mockup">
+			</div>
+		</div>
+		<button class="close-button" data-close aria-label="Close modal" type="button">
+			<span aria-hidden="true">&times;</span>
+		</button>
+	</div>
+</div>
+<div class="reveal row recent-projects-modal" id="houston-pals-modal" data-reveal>
+	<div class="medium-10 medium-push-1 columns end modal-container">
+		<div class="modal-header">
+			<h3>Houston PALS</h3>
+			<h4>Project Name</h4>
+		</div>
+		<div class="row collapse modal-prose">
+			<div class="large-7 columns">
+				<p>We’re currently in the middle of creating detailed project profile pages. In the meantime, why don’t you visit this project’s website?</p>
+			</div>
+			<div class="large-5 columns large-text-center">
+				<a class="button modal-site-link" href="http://houstonpals.com" target="_blank">www.HoustonPALS.com</a>
+			</div>
+		</div>
+		<div class="row collapse modal-images">
+			<div class="medium-6 columns">
+				<img src="/img/index/recent-projects/modals/houston-pals/hpals-1.png" alt="Houston PALS Desk Mockup">
+			</div>
+			<div class="medium-6 columns">
+				<img src="/img/index/recent-projects/modals/houston-pals/hpals-mobile-spread.png" alt="Houston PALS Mobile Device Spread">
 			</div>
 		</div>
 		<button class="close-button" data-close aria-label="Close modal" type="button">
