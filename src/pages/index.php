a8474fcdb68484c213513adc83ca20f8cb40ba01
Nico Watine <nico@studioncreations.com>
2016-11-08 11:29:25 -0600

    /modal content copied to index
    Merge and close

--- src/pages/index.php
+++ src/pages/index.php
@@ -54,7 +54,7 @@ body-class: index
 	<div class="row">
 		<div class="medium-6 columns">
 			<div class="project">
-				<a href="http://greencaretx.com/" target="_blank" title="Studio N Creations project link to GreencareTX.com"><img src="/img/index/recent-projects/greencaretx.png" alt="Greencare Interior Plants website mockup"></a>
+				<a data-open="greencare-tx-modal"><img src="/img/index/recent-projects/greencaretx.png" alt="Greencare Interior Plants website mockup"></a>
 			</div>
 		</div>
 		<div class="medium-6 columns">
@@ -244,6 +244,33 @@ body-class: index
 			</div>
 			<div class="medium-6 columns">
 				<img src="/img/index/recent-projects/modals/houston-pals/hpals-mobile-spread.png" alt="Houston PALS Mobile Device Spread">
+			</div>
+		</div>
+		<button class="close-button" data-close aria-label="Close modal" type="button">
+			<span aria-hidden="true">&times;</span>
+		</button>
+	</div>
+</div>
+<div class="reveal row recent-projects-modal" id="greencare-tx-modal" data-reveal>
+	<div class="medium-10 medium-push-1 columns end modal-container">
+		<div class="modal-header">
+			<h3>Greencare Interior Plants</h3>
+			<h4>Project Name</h4>
+		</div>
+		<div class="row collapse modal-prose">
+			<div class="large-7 columns">
+				<p>We’re currently in the middle of creating detailed project profile pages. In the meantime, why don’t you visit this project’s website?</p>
+			</div>
+			<div class="large-5 columns large-text-center">
+				<a class="button modal-site-link" href="http://greencaretx.com" target="_blank">www.GreencareTX.com</a>
+			</div>
+		</div>
+		<div class="row collapse modal-images">
+			<div class="medium-6 columns">
+				<img src="/img/index/recent-projects/modals/greencare-tx/greencare-2.png" alt="Houston PALS Desk Mockup">
+			</div>
+			<div class="medium-6 columns">
+				<img src="/img/index/recent-projects/modals/greencare-tx/greencare-1.png" alt="Houston PALS Mobile Device Spread">
 			</div>
 		</div>
 		<button class="close-button" data-close aria-label="Close modal" type="button">
