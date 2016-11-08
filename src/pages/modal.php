5a204464ca580182c991c730a874b876a54a499c
Nico Watine <nico@studioncreations.com>
2016-11-08 11:28:22 -0600

    GreencareTX modal and images completed
    Still need to optimize images

--- src/pages/modal.php
+++ src/pages/modal.php
@@ -8,12 +8,12 @@ og-image:
 body-id: modal-page
 body-class: modal-page
 ---
-<p><a data-open="houston-pals-modal">Click me for a modal</a></p>
+<p><a data-open="greencare-tx-modal">Click me for a modal</a></p>
 
-<div class="reveal row recent-projects-modal" id="houston-pals-modal" data-reveal>
+<div class="reveal row recent-projects-modal" id="greencare-tx-modal" data-reveal>
 	<div class="medium-10 medium-push-1 columns end modal-container">
 		<div class="modal-header">
-			<h3>Houston PALS</h3>
+			<h3>Greencare Interior Plants</h3>
 			<h4>Project Name</h4>
 		</div>
 		<div class="row collapse modal-prose">
@@ -21,15 +21,15 @@ body-class: modal-page
 				<p>We’re currently in the middle of creating detailed project profile pages. In the meantime, why don’t you visit this project’s website?</p>
 			</div>
 			<div class="large-5 columns large-text-center">
-				<a class="button modal-site-link" href="http://houstonpals.com" target="_blank">www.HoustonPALS.com</a>
+				<a class="button modal-site-link" href="http://greencaretx.com" target="_blank">www.GreencareTX.com</a>
 			</div>
 		</div>
 		<div class="row collapse modal-images">
 			<div class="medium-6 columns">
-				<img src="/img/index/recent-projects/modals/houston-pals/hpals-1.png" alt="Houston PALS Desk Mockup">
+				<img src="/img/index/recent-projects/modals/greencare-tx/greencare-2.png" alt="Houston PALS Desk Mockup">
 			</div>
 			<div class="medium-6 columns">
-				<img src="/img/index/recent-projects/modals/houston-pals/hpals-mobile-spread.png" alt="Houston PALS Mobile Device Spread">
+				<img src="/img/index/recent-projects/modals/greencare-tx/greencare-1.png" alt="Houston PALS Mobile Device Spread">
 			</div>
 		</div>
 		<button class="close-button" data-close aria-label="Close modal" type="button">
