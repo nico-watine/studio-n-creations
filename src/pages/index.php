4a89c4e3132b9e4192ba4138547918ead92f6b33
Nico Watine <nico@studioncreations.com>
2016-11-09 15:53:21 -0600

    Set up some h1 - h6 for visuals on index
    Also inserted correct font sizes for @small on _settings
    
    Switching back to index-main to confirm line-height of p in design

--- src/pages/index.php
+++ src/pages/index.php
@@ -8,3 +8,10 @@ og-image:
 body-id: index
 body-class:
 ---
+<h1>H1 - An Equal Amount Of Text</h1>
+<h2>H2 - An Equal Amount Of Text</h2>
+<h3>H3 - An Equal Amount Of Text</h3>
+<h4>H4 - An Equal Amount Of Text</h4>
+<h5>H5 - An Equal Amount Of Text</h5>
+<h6>H6 - An Equal Amount Of Text</h6>
+<p>P - An Equal Amount of Text lorem ipsum dolor</p>
