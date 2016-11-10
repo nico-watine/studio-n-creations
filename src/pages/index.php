7ee12a8a1b0b175855edcebf0015eab21b9433f8
Nico Watine <nico@studioncreations.com>
2016-11-10 15:11:30 -0600

    Renamed header elements and added custom styling
    Working up now, from smallest

--- src/pages/index.php
+++ src/pages/index.php
@@ -12,10 +12,10 @@ body-class: index
 	<div class="absolute-wrapper">
 		<div class="row">
 			<div class="medium-6 large-5 columns">
-				<h1>Studio N Creations</h1>
+				<h1 class="brand-wordmark">Studio N Creations</h1>
 			</div>
 			<div class="medium-6 large-7 columns">
-				<h2 class="index-header-tagline">Web Design <strong>|</strong> Graphic Branding <span class="inline-block"><strong>|</strong> Site Maintenance</span></h2>
+				<h2 class="brand-services">Web Design <strong>|</strong> Graphic Branding <span class="inline-block"><strong>|</strong> Site Maintenance</span></h2>
 			</div>
 		</div>
 	</div>
