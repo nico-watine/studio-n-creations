f02493af886d6ff213581b346a64f5bbd96b0060
Nico Watine <nico@studioncreations.com>
2016-11-21 23:28:20 -0600

    Got form and php file setup correct
    I think, but will need to text with the live server

---
+++ assets/php/sendContact.php
@@ -0,0 +1,12 @@
+<?php 
+$ToEmail = 'nico@studioncreations.com'; //ex. admin@scriptgates.com or scriptgates@gmail.com
+$EmailSubject = "SNC Message!!!"."\n"; 
+$mailheader = "From: ".$_POST["email"]."\n";
+$mailheader .= "Reply-To: ".$_POST["email"]."\n";  
+$MESSAGE_BODY = "Name: ".$_POST["name"]."\n"; 
+$MESSAGE_BODY .= "Email: ".$_POST["email"]."\n";
+$MESSAGE_BODY .= "Message: ".nl2br($_POST["message"])."\n"; 
+mail($ToEmail, $EmailSubject, $MESSAGE_BODY, $mailheader) or die ("Error!"); 
+
+// header("Location: /contact/thanks");
+?>
