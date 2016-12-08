1e1a163afb5597781e8220adad452cbd3d953c4e
Nico Watine <nico@studioncreations.com>
2016-12-08 17:54:52 -0600

    sendContact edited so now message breaks line
    Also added some spacing for visuals
    Still have no idea about the magic going on here

--- assets/php/sendContact.php
+++ assets/php/sendContact.php
@@ -6,8 +6,8 @@ $mailheader = "From: "."njwatine@gmail.com"."\n";
 $mailheader .= "Reply-To: "."njwatine@gmail.com"."\n";  
 // $MESSAGE_BODY = "Name: ".$_POST["name"]."\n"; 
 $MESSAGE_BODY = "Name: " . "\n" . $_POST["name"] . "\n"; 
-$MESSAGE_BODY .= "Email: " . "\n" . $_POST["email"]."\n";
-$MESSAGE_BODY .= "Message: " . "\n" . nl2br($_POST["message"])."\n"; 
+$MESSAGE_BODY .= "Email: " . "\n" . $_POST["email"] . "\n";
+$MESSAGE_BODY .= "Message: " . "\n" . $_POST["message"] . "\n"; 
 mail($ToEmail, $EmailSubject, $MESSAGE_BODY, $mailheader) or die ("Error!"); 
 
 header("Location: /#thank-you");
