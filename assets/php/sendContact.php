00d682f7fa613af4ed3ef4c80ec3eb5c1e0d2347
Nico Watine <nico@studioncreations.com>
2016-12-08 17:56:26 -0600

    sendContact edited for visuals
    STILL don’t know what’s happening here

--- assets/php/sendContact.php
+++ assets/php/sendContact.php
@@ -1,10 +1,8 @@
 <?php 
-$ToEmail = 'nico.w@tine.rocks'; //ex. admin@scriptgates.com or scriptgates@gmail.com
+$ToEmail = "nico.w@tine.rocks"; 
 $EmailSubject = "SNC Message!!!"."\n"; 
-// $mailheader = "From: ".$_POST["email"]."\n";
-$mailheader = "From: "."njwatine@gmail.com"."\n";
-$mailheader .= "Reply-To: "."njwatine@gmail.com"."\n";  
-// $MESSAGE_BODY = "Name: ".$_POST["name"]."\n"; 
+$mailheader = "From: " . "njwatine@gmail.com"."\n";
+$mailheader .= "Reply-To: " . "njwatine@gmail.com"."\n";  
 $MESSAGE_BODY = "Name: " . "\n" . $_POST["name"] . "\n"; 
 $MESSAGE_BODY .= "Email: " . "\n" . $_POST["email"] . "\n";
 $MESSAGE_BODY .= "Message: " . "\n" . $_POST["message"] . "\n"; 
