b5734fae90abf8fd537f3676d5d6fafaefb085ba
Nico Watine <nico@studioncreations.com>
2016-12-08 21:25:32 -0600

    Perfection of sendContact.php
    Way better Subject Line, so that it has a nice Re: look
    Sends to
    messages@studioncreations.com
    Reply from
    nico@studioncreations.com
    Reply to
    person’s actual email address

--- assets/php/sendContact.php
+++ assets/php/sendContact.php
@@ -1,8 +1,8 @@
 <?php 
 $ToEmail = "messages@studioncreations.com"; 
-$EmailSubject = "Studio N Creations Message"."\n"; 
-$mailheader = "From: " . "nico@studioncreations.com"."\n";
-$mailheader .= "Reply-To: " . "nico@studioncreations.com"."\n";  
+$EmailSubject = "Website Message!" . "\n"; 
+$mailheader = "From: " . "nico@studioncreations.com" . "\n";
+$mailheader .= "Reply-To: " . $_POST["email"] . "\n";  
 $MESSAGE_BODY = "Name: " . "\n" . $_POST["name"] . "\n"; 
 $MESSAGE_BODY .= "Email: " . "\n" . $_POST["email"] . "\n";
 $MESSAGE_BODY .= "Message: " . "\n" . $_POST["message"] . "\n"; 
