138205580236a205753ac21f002184b21863dfd0
Nico Watine <nico@studioncreations.com>
2016-12-08 20:52:36 -0600

    Change mailheader to send from nico@snc
    Will be easier to track
    Or should be open gmail address just for labeling these?

--- assets/php/sendContact.php
+++ assets/php/sendContact.php
@@ -1,8 +1,8 @@
 <?php 
 $ToEmail = "nico@studioncreations.com"; 
 $EmailSubject = "SNC Message!!!"."\n"; 
-$mailheader = "From: " . "njwatine@gmail.com"."\n";
-$mailheader .= "Reply-To: " . "njwatine@gmail.com"."\n";  
+$mailheader = "From: " . "nico@studioncreations.com"."\n";
+$mailheader .= "Reply-To: " . "nico@studioncreations.com"."\n";  
 $MESSAGE_BODY = "Name: " . "\n" . $_POST["name"] . "\n"; 
 $MESSAGE_BODY .= "Email: " . "\n" . $_POST["email"] . "\n";
 $MESSAGE_BODY .= "Message: " . "\n" . $_POST["message"] . "\n"; 
