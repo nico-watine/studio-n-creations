2daba455bd81611f73a43694f311fbe69983049e
Nico Watine <nico@studioncreations.com>
2016-12-08 21:18:05 -0600

    Changed sendContact email headers
    So now it goes TO
    messages@studioncreations.com
    which basically just goes to nico@snc, but this way all site form submissions can be easily categorized
    Also changed SUBJECT line bc going to make it easier to auto-respond to the actual submitted email address (do that next)

--- assets/php/sendContact.php
+++ assets/php/sendContact.php
@@ -1,8 +1,8 @@
 <?php 
 $ToEmail = "messages@studioncreations.com"; 
-$EmailSubject = "SNC Message!!!"."\n"; 
-$mailheader = "From: " . "messages@studioncreations.com"."\n";
-$mailheader .= "Reply-To: " . "messages@studioncreations.com"."\n";  
+$EmailSubject = "Studio N Creations Message"."\n"; 
+$mailheader = "From: " . "nico@studioncreations.com"."\n";
+$mailheader .= "Reply-To: " . "nico@studioncreations.com"."\n";  
 $MESSAGE_BODY = "Name: " . "\n" . $_POST["name"] . "\n"; 
 $MESSAGE_BODY .= "Email: " . "\n" . $_POST["email"] . "\n";
 $MESSAGE_BODY .= "Message: " . "\n" . $_POST["message"] . "\n"; 
