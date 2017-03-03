190f365ff793710656af6ecb7182687dd520743a
Nico Watine <nico@studioncreations.com>
2017-03-03 11:19:04 -0600

    HTML markup created for workflowCard template
    Now inline-style it up

--- src/pages/our-services/web-development.php
+++ src/pages/our-services/web-development.php
@@ -8,10 +8,24 @@ og-image:
 body-id: web-development
 body-class: web-development
 ---
-<section class="new-project-workflow">
-	<div class="row">
+<section class="new-project-workflow" style="padding-top: 40px;">
+	<div class="row" style="margin-bottom: 40px;">
 		<div class="large-7 large-push-2 columns end">
 			<h2>We offer a personalized website design and creation experience.</h2>
+			<h4>Our workflow for any new web project consists of 4 steps:</h4>
 		</div>
+	</div>
+	<div class="row">
+		<div class="large-6 columns">
+			<div class="workflowCard text-center">
+				<div class="workflowCard-title">
+					<h3>1. Site Layout Planning</h3>
+				</div>
+				<div class="workflowCard-description">
+					<p>This includes the content strategy of what info will be presented on the website.</p>
+				</div>
+			</div>
+		</div>
+		<div class="large-6 columns"></div>
 	</div>
 </section>
