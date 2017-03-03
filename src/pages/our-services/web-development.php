2d8a20f27cee9f97c09bc863ba3ecce88cb9031c
Nico Watine <nico@studioncreations.com>
2017-03-03 10:50:38 -0600

    Begin new-project-workflow with h2

--- src/pages/our-services/web-development.php
+++ src/pages/our-services/web-development.php
@@ -8,3 +8,10 @@ og-image:
 body-id: web-development
 body-class: web-development
 ---
+<section class="new-project-workflow">
+	<div class="row">
+		<div class="large-7 large-push-2 columns end">
+			<h2>We offer a personalized website design and creation experience.</h2>
+		</div>
+	</div>
+</section>
