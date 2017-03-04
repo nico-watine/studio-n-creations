a2fe18ea7eb8a5e928707128221fc323d15ff404
Nico Watine <nico@studioncreations.com>
2017-03-04 12:10:12 -0600

    Wrap text in .prose and add icon img

--- src/pages/our-services/web-development.php
+++ src/pages/our-services/web-development.php
@@ -80,12 +80,22 @@ body-class: web-development
 		</div>
 		<div class="row" style="margin-bottom: 20px;">
 			<div class="large-5 columns">
-				<h4>Hosting</h4>
-				<p>Once the site has been completed in full, we then host the website on our private servers and publish the site online.</p>
+				<div class="icon">
+					<img src="/img/our-services/web-development/new-project-workflow/server.png" alt="">
+				</div>
+				<div class="prose">
+					<h4>Hosting</h4>
+					<p>Once the site has been completed in full, we then host the website on our private servers and publish the site online.</p>
+				</div>
 			</div>
 			<div class="large-7 columns">
-				<h4>Maintenance</h4>
-				<p>After this, we discuss with our client about the extent to which they want to be involved in the site’s upkeep. We provide hassle-free site maintenace options so the client can focus on their business and not their website’s performance and reliability.</p>
+				<div class="icon">
+					<img src="/img/our-services/web-development/new-project-workflow/wrench.png" alt="">
+				</div>
+				<div class="prose">
+					<h4>Maintenance</h4>
+					<p>After this, we discuss with our client about the extent to which they want to be involved in the site’s upkeep. We provide hassle-free site maintenace options so the client can focus on their business and not their website’s performance and reliability.</p>
+				</div>
 			</div>
 		</div>
 	</div>
