e5e2c3b57ae508340bf07e975c2d8194792ef9ee
Nico Watine <nico@studioncreations.com>
2017-03-23 14:49:22 -0500

    Create default state top-nav with inline styles
    Also made entries in _nav.scss for the hamburger icon

--- src/pages/index.php
+++ src/pages/index.php
@@ -9,6 +9,9 @@ body-id: index
 body-class: index
 ---
 <header>
+
+	{{> top-nav}}
+
 	<div class="absolute-wrapper">
 		<div class="row">
 			<div class="medium-10 medium-push-1 large-5 large-push-0 columns end">
