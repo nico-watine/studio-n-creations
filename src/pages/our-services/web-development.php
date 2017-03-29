3159ca148738804dfb743f69f18e92683113f7ad
Nico Watine <nico@studioncreations.com>
2017-03-29 16:42:21 -0500

    Clean up all the indentation from web-development

--- src/pages/our-services/web-development.php
+++ src/pages/our-services/web-development.php
@@ -14,113 +14,114 @@ body-class: bodyLayout-default
 
 {{> offCanvas-pageStart}}
 
-			<section id="new-project-workflow">
-				<div class="row page-intro-prose">
-					<div class="medium-10 medium-push-1 large-7 large-push-2 columns end">
-						<h2>We offer a personalized website design and creation experience.</h2>
-						<h4>Our workflow for any new web project consists of 4 steps:</h4>
+<section id="new-project-workflow">
+	<div class="row page-intro-prose">
+		<div class="medium-10 medium-push-1 large-7 large-push-2 columns end">
+			<h2>We offer a personalized website design and creation experience.</h2>
+			<h4>Our workflow for any new web project consists of 4 steps:</h4>
+		</div>
+	</div>
+	<div class="row">
+		<div class="medium-10 medium-push-1 large-6 large-push-0 columns">
+			<div class="workflowCard text-center" id="site-layout-planning">
+				<div class="workflowCard-title">
+					<h3 class="textColor-white textShadow-black margin-0">1. Site Layout Planning</h3>
+				</div>
+				<div class="workflowCard-description">
+					<p class="textColor-white fontWeight-400 margin-0">This includes the content strategy of what info will be presented <span class="inline-block">on the website.</span></p>
+				</div>
+			</div>
+		</div>
+		<div class="medium-10 medium-push-1 large-6 large-push-0 end columns">
+			<div class="workflowCard text-center" id="design-phase">
+				<div class="workflowCard-title">
+					<h3 class="margin-0">2. Design Phase</h3>
+				</div>
+				<div class="workflowCard-description">
+					<p class="textColor-white fontWeight-400 margin-0">We consult with our clients closely throughout the design process to ensure full satisfaction with the site’s look and feel.</p>
+				</div>
+			</div>
+		</div>
+	</div>
+	<div class="row">
+		<div class="large-8 large-push-2 columns end">
+			<div class="workflowCard text-center" id="coding-development-stage">
+				<div class="workflowCard-title">
+					<h3 class="textColor-white textShadow-black margin-0">3. Coding &amp; Development Stage</h3>
+				</div>
+				<div class="workflowCard-description">
+					<p class="textColor-white fontWeight-400 margin-0">We then code the approved design into a functioning website, using the industry’s latest <span class="inline-block">methods and standards.</span></p>
+				</div>
+			</div>
+		</div>
+	</div>
+	<div class="row-expanded" id="post-production-actions">
+		<div class="row">
+			<div class="columns">
+				<h3 class="text-center">4. Post-Production Actions</h3>
+			</div>
+		</div>
+		<div class="row" style="margin-bottom: 20px;">
+			<div class="medium-5 columns">
+				<div class="icon">
+					<img src="{{ cdn }}/img/our-services/web-development/new-project-workflow/server.svg" alt="Hosting Server Icon" title="Server Hosting Icon">
+				</div>
+				<div class="prose text-center medium-text-left">
+					<h4>Hosting</h4>
+					<p>Once the site has been completed in full, we then host the website on our private servers and publish the site online.</p>
+				</div>
+			</div>
+			<div class="medium-7 columns">
+				<div class="icon">
+					<img src="{{ cdn }}/img/our-services/web-development/new-project-workflow/wrench.svg" alt="Maintenance Wrench" title="Maintenance Wrench Icon">
+				</div>
+				<div class="prose text-center medium-text-left">
+					<h4>Maintenance</h4>
+					<p>After this, we discuss with our client about the extent to which they want to be involved in the site’s upkeep. We provide hassle-free site maintenace options so the client can focus on their business and not their website’s performance and reliability.</p>
+				</div>
+			</div>
+		</div>
+	</div>
+</section>
+<section id="design-into-code-conversion" class="textColor-white">
+	<div class="row">
+		<div class="medium-push-1 medium-11 large-push-2 large-10 columns">
+			<h5 class="subHeading monospace">Service Offered</h5>
+			<h2 class="heading"><span class="monospace">Design into Code</span> conversion</h2>
+		</div>
+	</div>
+	<div class="row">
+		<div class="medium-3 large-push-2 large-2 columns medium-text-right large-text-center show-for-medium">
+			<img class="html5-logo" src="{{ cdn }}/img/our-services/web-development/design-into-code-conversion/html5-logo.svg" alt="HTML5 Logo" title="HTML5 Logo">
+		</div>
+		<div class="medium-9 large-8 columns">
+			<h4>If you already have a collection of web page mockups, we can transform the designs into a fully-coded, functioning website.</h4>
+			<div class="row nestedRow">
+				<div class="hide-for-medium text-center">
+					<div class="small-4 columns">
+						<img class="html5-logo" src="{{ cdn }}/img/our-services/web-development/design-into-code-conversion/html5-logo-square.svg" alt="HTML5 Logo" title="HTML5 Logo">
+					</div>
+					<div class="small-4 columns">
+						<img srcset="{{ cdn }}/img/our-services/web-development/design-into-code-conversion/photoshop_2x.png 2x" src="{{ cdn }}/img/our-services/web-development/design-into-code-conversion/photoshop.png" alt="Adobe Photoshop logo" title="Adobe Photoshop logo">
+					</div>
+					<div class="small-4 columns">
+						<img srcset="{{ cdn }}/img/our-services/web-development/design-into-code-conversion/sketch-app_2x.png 2x" src="{{ cdn }}/img/our-services/web-development/design-into-code-conversion/sketch-app.png" alt="Sketch App logo" title="Sketch App logo">
 					</div>
 				</div>
-				<div class="row">
-					<div class="medium-10 medium-push-1 large-6 large-push-0 columns">
-						<div class="workflowCard text-center" id="site-layout-planning">
-							<div class="workflowCard-title">
-								<h3 class="textColor-white textShadow-black margin-0">1. Site Layout Planning</h3>
-							</div>
-							<div class="workflowCard-description">
-								<p class="textColor-white fontWeight-400 margin-0">This includes the content strategy of what info will be presented <span class="inline-block">on the website.</span></p>
-							</div>
-						</div>
+				<div class="medium-6 columns">
+					<p class="fontWeight-400">We work with both <strong>Photoshop</strong> and <strong>Sketch App</strong> when converting files into functioning <strong>HTML5</strong>.</p>
+				</div>
+				<div class="show-for-medium">
+					<div class="small-6 medium-3 columns">
+						<img srcset="{{ cdn }}/img/our-services/web-development/design-into-code-conversion/photoshop_2x.png 2x" src="{{ cdn }}/img/our-services/web-development/design-into-code-conversion/photoshop.png" alt="Adobe Photoshop logo" title="Adobe Photoshop logo">
 					</div>
-					<div class="medium-10 medium-push-1 large-6 large-push-0 end columns">
-						<div class="workflowCard text-center" id="design-phase">
-							<div class="workflowCard-title">
-								<h3 class="margin-0">2. Design Phase</h3>
-							</div>
-							<div class="workflowCard-description">
-								<p class="textColor-white fontWeight-400 margin-0">We consult with our clients closely throughout the design process to ensure full satisfaction with the site’s look and feel.</p>
-							</div>
-						</div>
+					<div class="small-6 medium-3 columns">
+						<img srcset="{{ cdn }}/img/our-services/web-development/design-into-code-conversion/sketch-app_2x.png 2x" src="{{ cdn }}/img/our-services/web-development/design-into-code-conversion/sketch-app.png" alt="Sketch App logo" title="Sketch App logo">
 					</div>
 				</div>
-				<div class="row">
-					<div class="large-8 large-push-2 columns end">
-						<div class="workflowCard text-center" id="coding-development-stage">
-							<div class="workflowCard-title">
-								<h3 class="textColor-white textShadow-black margin-0">3. Coding &amp; Development Stage</h3>
-							</div>
-							<div class="workflowCard-description">
-								<p class="textColor-white fontWeight-400 margin-0">We then code the approved design into a functioning website, using the industry’s latest <span class="inline-block">methods and standards.</span></p>
-							</div>
-						</div>
-					</div>
-				</div>
-				<div class="row-expanded" id="post-production-actions">
-					<div class="row">
-						<div class="columns">
-							<h3 class="text-center">4. Post-Production Actions</h3>
-						</div>
-					</div>
-					<div class="row" style="margin-bottom: 20px;">
-						<div class="medium-5 columns">
-							<div class="icon">
-								<img src="{{ cdn }}/img/our-services/web-development/new-project-workflow/server.svg" alt="Hosting Server Icon" title="Server Hosting Icon">
-							</div>
-							<div class="prose text-center medium-text-left">
-								<h4>Hosting</h4>
-								<p>Once the site has been completed in full, we then host the website on our private servers and publish the site online.</p>
-							</div>
-						</div>
-						<div class="medium-7 columns">
-							<div class="icon">
-								<img src="{{ cdn }}/img/our-services/web-development/new-project-workflow/wrench.svg" alt="Maintenance Wrench" title="Maintenance Wrench Icon">
-							</div>
-							<div class="prose text-center medium-text-left">
-								<h4>Maintenance</h4>
-								<p>After this, we discuss with our client about the extent to which they want to be involved in the site’s upkeep. We provide hassle-free site maintenace options so the client can focus on their business and not their website’s performance and reliability.</p>
-							</div>
-						</div>
-					</div>
-				</div>
-			</section>
-			<section id="design-into-code-conversion" class="textColor-white">
-				<div class="row">
-					<div class="medium-push-1 medium-11 large-push-2 large-10 columns">
-						<h5 class="subHeading monospace">Service Offered</h5>
-						<h2 class="heading"><span class="monospace">Design into Code</span> conversion</h2>
-					</div>
-				</div>
-				<div class="row">
-					<div class="medium-3 large-push-2 large-2 columns medium-text-right large-text-center show-for-medium">
-						<img class="html5-logo" src="{{ cdn }}/img/our-services/web-development/design-into-code-conversion/html5-logo.svg" alt="HTML5 Logo" title="HTML5 Logo">
-					</div>
-					<div class="medium-9 large-8 columns">
-						<h4>If you already have a collection of web page mockups, we can transform the designs into a fully-coded, functioning website.</h4>
-						<div class="row nestedRow">
-							<div class="hide-for-medium text-center">
-								<div class="small-4 columns">
-									<img class="html5-logo" src="{{ cdn }}/img/our-services/web-development/design-into-code-conversion/html5-logo-square.svg" alt="HTML5 Logo" title="HTML5 Logo">
-								</div>
-								<div class="small-4 columns">
-									<img srcset="{{ cdn }}/img/our-services/web-development/design-into-code-conversion/photoshop_2x.png 2x" src="{{ cdn }}/img/our-services/web-development/design-into-code-conversion/photoshop.png" alt="Adobe Photoshop logo" title="Adobe Photoshop logo">
-								</div>
-								<div class="small-4 columns">
-									<img srcset="{{ cdn }}/img/our-services/web-development/design-into-code-conversion/sketch-app_2x.png 2x" src="{{ cdn }}/img/our-services/web-development/design-into-code-conversion/sketch-app.png" alt="Sketch App logo" title="Sketch App logo">
-								</div>
-							</div>
-							<div class="medium-6 columns">
-								<p class="fontWeight-400">We work with both <strong>Photoshop</strong> and <strong>Sketch App</strong> when converting files into functioning <strong>HTML5</strong>.</p>
-							</div>
-							<div class="show-for-medium">
-								<div class="small-6 medium-3 columns">
-									<img srcset="{{ cdn }}/img/our-services/web-development/design-into-code-conversion/photoshop_2x.png 2x" src="{{ cdn }}/img/our-services/web-development/design-into-code-conversion/photoshop.png" alt="Adobe Photoshop logo" title="Adobe Photoshop logo">
-								</div>
-								<div class="small-6 medium-3 columns">
-									<img srcset="{{ cdn }}/img/our-services/web-development/design-into-code-conversion/sketch-app_2x.png 2x" src="{{ cdn }}/img/our-services/web-development/design-into-code-conversion/sketch-app.png" alt="Sketch App logo" title="Sketch App logo">
-								</div>
-							</div>
-						</div>
-					</div>
-				</div>
-			</section>
+			</div>
+		</div>
+	</div>
+</section>
+
 {{> offCanvas-pageEnd}}
