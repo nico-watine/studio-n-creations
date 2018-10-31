d947807bebf1ccc07ef27da69d1d8258811a8139
Nico Watine <nico@studioncreations.com>
2018-10-31 11:56:30 -0500

    Add class to index body

--- src/pages/index.php
+++ src/pages/index.php
@@ -6,7 +6,7 @@ og-title: Web Design & Site Maintenance | Studio N Creations
 og-description: We create phone-friendly websites for projects of all sizes. From the design and branding to the coding and maintenance, we've got your web needs covered. Located in Houston, TX, available anywhere.
 og-image: /img/assets/og-images/index/studio-n-creations-og-image-1.png
 body-id: index
-body-class: bodyLayout-unique
+body-class: index bodyLayout-unique
 ---
 <header class="grid-x" style="position: relative; overflow: hidden;">
 	<div class="grid-container">
