c4d9a6f299646d4674b9353932a1dfc00b546fe2
Nico Watine <nico@studioncreations.com>
2018-12-27 16:49:23 -0600

    Add CDN variable

--- src/pages/index.php
+++ src/pages/index.php
@@ -21,10 +21,10 @@ body-class: index bodyLayout-unique
 	</div>
 	<div class="grid-x position-absolute w-100 h-100" style="width: 100%; height: 100%;">
 		<div class="cell">
-			<video playsinline autoplay muted loop preload="auto" poster="/img/index/website-mashup-video/studio-n-creations-website-mashup-poster.jpg" id="backgroundVideo">
-				<source src="/img/index/website-mashup-video/studio-n-creations-website-mashup.mp4" type="video/mp4">
-				<source src="/img/index/website-mashup-video/studio-n-creations-website-mashup.webm" type="video/webm">
-				<source src="/img/index/website-mashup-video/studio-n-creations-website-mashup.ogv" type="video/ogg">
+			<video playsinline autoplay muted loop preload="auto" poster="{{ cdn }}/img/index/website-mashup-video/studio-n-creations-website-mashup-poster.jpg" id="backgroundVideo">
+				<source src="{{ cdn }}/img/index/website-mashup-video/studio-n-creations-website-mashup.mp4" type="video/mp4">
+				<source src="{{ cdn }}/img/index/website-mashup-video/studio-n-creations-website-mashup.webm" type="video/webm">
+				<source src="{{ cdn }}/img/index/website-mashup-video/studio-n-creations-website-mashup.ogv" type="video/ogg">
 			</video>
 		</div>
 	</div>
