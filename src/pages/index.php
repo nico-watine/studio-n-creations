ef69f28427a43d0400df1b8912bcaec29062dd58
Nico Watine <nico@studioncreations.com>
2022-04-09 15:07:22 -0500

    Update about with new prose and tweak scss

--- src/pages/index.php
+++ src/pages/index.php
@@ -181,13 +181,14 @@ body-class: index bodyLayout-unique
 				<img srcset="{{ cdn }}/img/index/about-us/hello-lanyard_2x.jpg 2x" src="{{ cdn }}/img/index/about-us/hello-lanyard.jpg" alt="Nico Watine is Studio N Creations" title="HELLO my name is Nico Watine" itemprop="image">
 			</div>
 			<div class="small-6 columns hide-for-medium">
-				<h3>About Us</h3>
+				<h3>About Me</h3>
 			</div>
 		</div>
 		<div class="medium-8 large-9 columns prose">
 			<h3 class="show-for-medium">About Me</h3>
-			<h4>My name is <span itemprop="name">Nico Watine</span>, and I am the <span class="display-inline-block" itemprop="affiliation">Studio N Creations</span> team.</h4>
-			<p>I design, brand, code and maintain mobile-optimized websites for projects of all sizes. I’m located in Houston, TX, and am available for hire anywhere. I'm a <span itemprop="jobTitle">web developer</span> at heart and am passionate about what I do. Get in touch and we'll get started.</p>
+			<h4 class="mb-16">My name is <span itemprop="name">Nico Watine</span>, and I am the <span class="display-inline-block" itemprop="affiliation">Studio N Creations</span> team. I design, code and maintain mobile-optimized websites for projects of all sizes.</h4>
+			<p>Having a professional website is key to a company’s image and success, and that’s exactly what I specialize in — strong websites for B2B and B2C small businesses. While I used to do full-stack web development full time, I now offer my services as a part-time <span itemprop="jobTitle">freelance web designer</span>. I only take on 1 or 2 projects at a time to ensure I can provide the utmost excellence in service and quality with every job, for every client.</p>
+			<p>I’m located in Houston, TX, and am available for hire anywhere. Please contact me and let’s see when we can get started — I’m only a message away.</p>
 		</div>
 	</div>
 </section>
