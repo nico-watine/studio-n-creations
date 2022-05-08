cc24ba0a8101e724aba85fa0190b216efdcac956
Nico Watine <nico@studioncreations.com>
2022-05-08 18:03:31 -0500

    Update alt tags

--- src/pages/projects.php
+++ src/pages/projects.php
@@ -17,57 +17,57 @@ body-class: index bodyLayout-unique
 	<div id="masonryGrid" class="row small-up-2 medium-up-3 large-up-4">
 		<div class="tile column column-block">
 			<div class="tileBase">
-				<a href="/img/projects/_dev/greencare.png"><img src="/img/projects/_dev/greencare.png" alt="Greencare Interior Plants Gallery Image - 1"></a>
+				<a href="/img/projects/_dev/greencare.png"><img src="/img/projects/_dev/greencare.png" alt="Greencare Interior Plants"></a>
 			</div>
 		</div>
 		<div class="tile column column-block">
 			<div class="tileBase">
-				<a href="/img/projects/_dev/miriam-rodriguez.png"><img src="/img/projects/_dev/miriam-rodriguez.png" alt="Greencare Interior Plants Gallery Image - 2"></a>
+				<a href="/img/projects/_dev/miriam-rodriguez.png"><img src="/img/projects/_dev/miriam-rodriguez.png" alt="Miriam Rodriguez Couturier"></a>
 			</div>
 		</div>
 		<div class="tile column column-block">
 			<div class="tileBase">
-				<a href="/img/projects/_dev/excess.png"><img src="/img/projects/_dev/excess.png" alt="Greencare Interior Plants Gallery Image - 3"></a>
+				<a href="/img/projects/_dev/excess.png"><img src="/img/projects/_dev/excess.png" alt="Excess Fields"></a>
 			</div>
 		</div>
 		<div class="tile column column-block">
 			<div class="tileBase">
-				<a href="/img/projects/_dev/nicolas-watine-fr.png"><img src="/img/projects/_dev/nicolas-watine-fr.png" alt="Greencare Interior Plants Gallery Image - 4"></a>
+				<a href="/img/projects/_dev/nicolas-watine-fr.png"><img src="/img/projects/_dev/nicolas-watine-fr.png" alt="Artist Nicolas Watine FR"></a>
 			</div>
 		</div>
 		<div class="tile column column-block">
 			<div class="tileBase">
-				<a href="/img/projects/_dev/lemon-peel-facial.png"><img src="/img/projects/_dev/lemon-peel-facial.png" alt="Greencare Interior Plants Gallery Image - 5"></a>
+				<a href="/img/projects/_dev/lemon-peel-facial.png"><img src="/img/projects/_dev/lemon-peel-facial.png" alt="Lemon Peel Facial"></a>
 			</div>
 		</div>
 		<div class="tile column column-block">
 			<div class="tileBase">
-				<a href="/img/projects/_dev/tacupra.png"><img src="/img/projects/_dev/tacupra.png" alt="Greencare Interior Plants Gallery Image - 6"></a>
+				<a href="/img/projects/_dev/tacupra.png"><img src="/img/projects/_dev/tacupra.png" alt="Tacupra"></a>
 			</div>
 		</div>
 		<div class="tile column column-block">
 			<div class="tileBase">
-				<a href="/img/projects/_dev/houston-pals.png"><img src="/img/projects/_dev/houston-pals.png" alt="Greencare Interior Plants Gallery Image - 7"></a>
+				<a href="/img/projects/_dev/houston-pals.png"><img src="/img/projects/_dev/houston-pals.png" alt="Houston PALS"></a>
 			</div>
 		</div>
 		<div class="tile column column-block">
 			<div class="tileBase">
-				<a href="/img/projects/_dev/julia-garrison.png"><img src="/img/projects/_dev/julia-garrison.png" alt="Greencare Interior Plants Gallery Image - 7"></a>
+				<a href="/img/projects/_dev/julia-garrison.png"><img src="/img/projects/_dev/julia-garrison.png" alt="Julia & Garrison"></a>
 			</div>
 		</div>
 		<div class="tile column column-block">
 			<div class="tileBase">
-				<a href="/img/projects/_dev/houston-uncovered.png"><img src="/img/projects/_dev/houston-uncovered.png" alt="Greencare Interior Plants Gallery Image - 7"></a>
+				<a href="/img/projects/_dev/houston-uncovered.png"><img src="/img/projects/_dev/houston-uncovered.png" alt="Houston Uncovered"></a>
 			</div>
 		</div>
 		<div class="tile column column-block">
 			<div class="tileBase">
-				<a href="/img/projects/_dev/zaraza-design.png"><img src="/img/projects/_dev/zaraza-design.png" alt="Greencare Interior Plants Gallery Image - 7"></a>
+				<a href="/img/projects/_dev/zaraza-design.png"><img src="/img/projects/_dev/zaraza-design.png" alt="Zaraza Design"></a>
 			</div>
 		</div>
 		<div class="tile column column-block">
 			<div class="tileBase">
-				<a href="/img/projects/_dev/starhill-farms.png"><img src="/img/projects/_dev/starhill-farms.png" alt="Greencare Interior Plants Gallery Image - 7"></a>
+				<a href="/img/projects/_dev/starhill-farms.png"><img src="/img/projects/_dev/starhill-farms.png" alt="Starhill Farms"></a>
 			</div>
 		</div>
 	</div>
