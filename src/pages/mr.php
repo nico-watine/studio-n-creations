0e7937f6f5f87ca98bcb9b0bae2dc56e5b67b53b
Nico Watine <nico@studioncreations.com>
2022-06-12 17:21:58 -0500

    Move all inline styles to scss

--- src/pages/mr.php
+++ src/pages/mr.php
@@ -8,19 +8,19 @@ og-image: /img/assets/og-images/index/studio-n-creations-og-image-1.png
 body-id: index
 body-class: index bodyLayout-unique
 ---
-<section class="projectBanner overflow-hidden" id="miriam-rodriguez" style="position: relative; padding-top: 50px; height: 400px;">
-	<div class="row" style="position: relative; z-index: 10;">
+<section class="projectBanner overflow-hidden position-relative" id="miriam-rodriguez">
+	<div class="row position-relative" style="z-index: 10;">
 		<div class="medium-6 columns">
 			<div class="prose-wrap color-white display-inline-block float-right">
 				<h3 class="monospace text-center mb-20">Miriam Rodríguez</h3>
-				<p style="font-size: 18px; max-width: 400px;">International bridal couturier Miriam Rodríguez hired me to create her bilingual online portfolio so she could expand her business and capitalize on her booming Instagram profile.</p>
+				<p>International bridal couturier Miriam Rodríguez hired me to create her bilingual online portfolio so she could expand her business and capitalize on her booming Instagram profile.</p>
 			</div>
 		</div>
 		<div class="medium-6 columns">
 			<div class="img-wrap">
-				<img style="max-width: 475px;" src="/img/projects/miriam-rodriguez/miriam-rodriguez.png" alt="">
+				<img src="/img/projects/miriam-rodriguez/miriam-rodriguez.png" alt="">
 			</div>
 		</div>
 	</div>
-	<img class="box-shadow-5" style="position: absolute; width: 490px; top: -575px; right: 0;" src="/img/projects/miriam-rodriguez/miriam-rodriguez.png" alt="">
+	<img class="bg-screenshot" src="/img/projects/miriam-rodriguez/miriam-rodriguez.png" alt="">
 </section>
