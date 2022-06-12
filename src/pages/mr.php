65dd498b8d02d218e280b29d68d4b7b7a823f7cd
Nico Watine <nico@studioncreations.com>
2022-06-12 17:02:47 -0500

    Get basic structure without button

--- src/pages/mr.php
+++ src/pages/mr.php
@@ -8,6 +8,14 @@ og-image: /img/assets/og-images/index/studio-n-creations-og-image-1.png
 body-id: index
 body-class: index bodyLayout-unique
 ---
-<section class="projectBanner" id="miriam-rodriguez">
-&nbsp;
+<section class="projectBanner" id="miriam-rodriguez" style="padding-top: 50px; padding-bottom: 50px;">
+	<div class="row">
+		<div class="medium-6 columns">
+			<div class="prose-wrap color-white display-inline-block float-right">
+				<h3 class="monospace text-center mb-20">Miriam Rodríguez</h3>
+				<p style="font-size: 18px; max-width: 400px;">International bridal couturier Miriam Rodríguez hired me to create her bilingual online portfolio so she could expand her business and capitalize on her booming Instagram profile.</p>
+			</div>
+		</div>
+		<div class="medium-6 columns"></div>
+	</div>
 </section>
