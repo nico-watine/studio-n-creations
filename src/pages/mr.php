23d72e0ec20dff4bf32151b3df21a5d1d1a00a51
Nico Watine <nico@studioncreations.com>
2022-06-12 16:31:34 -0500

    Set up base files

---
+++ src/pages/mr.php
@@ -0,0 +1,13 @@
+---
+url: /
+title: Web Design & Site Maintenance
+description: We design, brand, code and maintain mobile-optimized websites for projects of all sizes. Located in Houston, TX, available anywhere.
+og-title: Web Design & Site Maintenance | Studio N Creations
+og-description: We create phone-friendly websites for projects of all sizes. From the design and branding to the coding and maintenance, we've got your web needs covered. Located in Houston, TX, available anywhere.
+og-image: /img/assets/og-images/index/studio-n-creations-og-image-1.png
+body-id: index
+body-class: index bodyLayout-unique
+---
+<section class="projectBanner" id="miriam-rodriguez">
+
+</section>
