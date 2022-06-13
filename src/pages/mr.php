ca29c264a94c57559d3ae2b6ece9b1ad3ad4ec13
Nico Watine <nico@studioncreations.com>
2022-06-13 12:50:10 -0500

    Playing with image bg

--- src/pages/mr.php
+++ src/pages/mr.php
@@ -9,19 +9,21 @@ body-id: index
 body-class: index bodyLayout-unique
 ---
 <section class="projectBanner overflow-hidden position-relative" id="miriam-rodriguez">
-	<div class="row position-relative" style="z-index: 10;">
-		<div class="medium-6 columns text-center medium-text-left">
-			<div class="prose-wrap color-white display-inline-block text-left">
-				<h3 class="monospace text-center mb-20">Miriam Rodríguez</h3>
-				<p>International bridal couturier Miriam Rodríguez hired me to create her bilingual online portfolio so she could expand her business and capitalize on her booming <a href="https://instagram.com/realmiriamrodriguez?igshid=YmMyMTA2M2Y=" target="_blank">Instagram profile</a>.</p>
-				<div class="text-center">
-					<a href="https://miriamrodriguez.com/" class="site-link">www.miriamrodriguez.com</a>
+	<div class="bg-blur position-relative">
+		<div class="row">
+			<div class="medium-6 columns text-center medium-text-left">
+				<div class="prose-wrap color-white display-inline-block text-left">
+					<h3 class="monospace text-center mb-20">Miriam Rodríguez</h3>
+					<p>International bridal couturier Miriam Rodríguez hired me to create her bilingual online portfolio so she could expand her business and capitalize on her booming <a href="https://instagram.com/realmiriamrodriguez?igshid=YmMyMTA2M2Y=" target="_blank">Instagram profile</a>.</p>
+					<div class="text-center">
+						<a href="https://miriamrodriguez.com/" class="site-link">www.miriamrodriguez.com</a>
+					</div>
 				</div>
 			</div>
-		</div>
-		<div class="medium-6 columns">
-			<div class="img-wrap show-for-medium">
-				<img src="/img/projects/miriam-rodriguez/miriam-rodriguez.png" alt="Screenshot of miriamrodriguez.com">
+			<div class="medium-6 columns">
+				<div class="img-wrap show-for-medium">
+					<img src="/img/projects/miriam-rodriguez/miriam-rodriguez.png" alt="Screenshot of miriamrodriguez.com">
+				</div>
 			</div>
 		</div>
 	</div>
