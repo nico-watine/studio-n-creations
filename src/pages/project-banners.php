9f5f71c684408a96727e7397227e5fa9eda2028b
Nico Watine <nico@studioncreations.com>
2022-07-04 10:56:21 -0500

    Group into new single page

---
+++ src/pages/project-banners.php
@@ -0,0 +1,53 @@
+---
+url: /
+title: Web Design & Site Maintenance
+description: We design, brand, code and maintain mobile-optimized websites for projects of all sizes. Located in Houston, TX, available anywhere.
+og-title: Web Design & Site Maintenance | Studio N Creations
+og-description: We create phone-friendly websites for projects of all sizes. From the design and branding to the coding and maintenance, we've got your web needs covered. Located in Houston, TX, available anywhere.
+og-image: /img/assets/og-images/index/studio-n-creations-og-image-1.png
+body-id: index
+body-class: index bodyLayout-unique
+---
+<section class="projectBanner overflow-hidden position-relative" id="miriam-rodriguez">
+	<div class="bg-blur position-relative">
+		<div class="row">
+			<div class="medium-6 columns text-center medium-text-left">
+				<div class="prose-wrap color-white display-inline-block text-left">
+					<h3 class="monospace text-center mb-20">Miriam Rodríguez</h3>
+					<p>International bridal couturier Miriam Rodríguez hired me to create her bilingual online portfolio so she could expand her business and capitalize on her booming <a href="https://instagram.com/realmiriamrodriguez?igshid=YmMyMTA2M2Y=" target="_blank">Instagram profile</a>.</p>
+					<div class="text-center">
+						<a href="https://miriamrodriguez.com/" class="site-link">www.miriamrodriguez.com</a>
+					</div>
+				</div>
+			</div>
+			<div class="medium-6 columns">
+				<div class="img-wrap show-for-medium">
+					<img src="/img/projects/miriam-rodriguez/miriam-rodriguez.png" alt="Screenshot of miriamrodriguez.com">
+				</div>
+			</div>
+		</div>
+		<img class="bg-screenshot" style="user-select: none;pointer-events: none;" src="/img/projects/miriam-rodriguez/miriam-rodriguez.png" alt="">
+	</div>
+</section>
+
+<section class="projectBanner overflow-hidden position-relative" id="hpals">
+	<div class="bg-blur position-relative">
+		<div class="row">
+			<div class="medium-6 columns">
+				<div class="img-wrap float-right show-for-medium">
+					<img src="/img/projects/houston-pals/houston-pals.png" alt="Screenshot of houstonpals.com">
+				</div>
+			</div>
+			<div class="medium-6 columns text-center medium-text-left">
+				<div class="prose-wrap color-white display-inline-block text-left">
+					<h3 class="monospace text-center mb-20">Houston PALS</h3>
+					<p>I worked with Bill Bickley of Houston Pet Artifical Limbs & Supports to craft a professional online presence for his specialized animal veterinary business in time for Puppy Bowl 15.</p>
+					<div class="text-center">
+						<a href="https://houstonpals.com/" class="site-link">www.houstonpals.com</a>
+					</div>
+				</div>
+			</div>
+		</div>
+		<img class="bg-screenshot" style="user-select: none;pointer-events: none;" src="/img/projects/houston-pals/houston-pals.png" alt="">
+	</div>
+</section>
