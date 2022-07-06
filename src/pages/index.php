772d3cda4faefe7924f474b9bc25c3e43a084920
Nico Watine <nico@studioncreations.com>
2022-07-06 09:28:31 -0500

    Move project banners to index

--- src/pages/index.php
+++ src/pages/index.php
@@ -56,6 +56,54 @@ body-class: index bodyLayout-unique
 	</div>
 </section>
 
+<section class="projectBanner overflow-hidden position-relative" id="miriam-rodriguez">
+	<div class="bg-blur position-relative">
+		<div class="row">
+			<div class="medium-6 columns text-center medium-text-left">
+				<div class="prose-wrap color-white display-inline-block text-left">
+					<h3 class="monospace text-center mb-20">Miriam Rodríguez</h3>
+					<p>International bridal couturier Miriam Rodríguez hired me to create her bilingual online portfolio so she could expand her business and capitalize on her booming <a href="https://instagram.com/realmiriamrodriguez?igshid=YmMyMTA2M2Y=" target="_blank">Instagram profile</a>.</p>
+					<div class="text-center">
+						<a href="https://miriamrodriguez.com/" class="site-link">www.miriamrodriguez.com</a>
+					</div>
+				</div>
+			</div>
+			<div class="medium-6 columns">
+				<div class="img-wrap show-for-medium">
+					<img src="/img/projects/miriam-rodriguez/miriam-rodriguez.png" alt="Screenshot of miriamrodriguez.com">
+				</div>
+			</div>
+		</div>
+		<img class="bg-screenshot" style="user-select: none;pointer-events: none;" src="/img/projects/miriam-rodriguez/miriam-rodriguez.png" alt="">
+	</div>
+</section>
+
+<div class="py-8" style="background: #566372;">
+	<hr>
+</div>
+
+<section class="projectBanner overflow-hidden position-relative" id="hpals">
+	<div class="bg-blur position-relative">
+		<div class="row">
+			<div class="medium-6 columns">
+				<div class="img-wrap float-right show-for-medium">
+					<img src="/img/projects/houston-pals/houston-pals.png" alt="Screenshot of houstonpals.com">
+				</div>
+			</div>
+			<div class="medium-6 columns text-center medium-text-left">
+				<div class="prose-wrap color-white display-inline-block text-left">
+					<h3 class="monospace text-center mb-20">Houston PALS</h3>
+					<p>I worked with Bill Bickley of Houston Pet Artifical Limbs & Supports to craft a professional online presence for his specialized animal veterinary business in time for Puppy Bowl 15.</p>
+					<div class="text-center">
+						<a href="https://houstonpals.com/" class="site-link">www.houstonpals.com</a>
+					</div>
+				</div>
+			</div>
+		</div>
+		<img class="bg-screenshot" style="user-select: none;pointer-events: none;" src="/img/projects/houston-pals/houston-pals.png" alt="">
+	</div>
+</section>
+
 <!-- <section class="gallery-section">
 	<div class="row">
 		<div class="small-12 columns text-center">
