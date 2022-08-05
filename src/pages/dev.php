58fb5d409a16b0f794d16d54af71e81b2f122d3b
Nico Watine <nico@studioncreations.com>
2022-08-05 15:14:01 -0500

    New scss partial for dev

--- src/pages/dev.php
+++ src/pages/dev.php
@@ -8,3 +8,29 @@ og-image: /img/assets/og-images/index/studio-n-creations-og-image-1.png
 body-id: index
 body-class: index bodyLayout-unique
 ---
+<section class="graphic-branding">
+	<div class="row collapse">
+		<div class="medium-push-1 medium-11 large-4 large-push-0 columns">
+			<div class="prose">
+				<h3>Graphic Branding</h3>
+				<p>Web design means more than just a page design. Having a solid visual brand, such as through logos and colors, helps maintain design consistency, leaving a professional impression on the visitor. Here are a few of the custom logos I have created for clients.</p>
+			</div>
+		</div>
+		<div class="medium-12 large-8 columns">
+			<div class="row logos-row collapse">
+				<div class="medium-4 columns text-center column-1">
+					<div class="dynasty-sports-report-wrapper">
+						<img src="/img/projects/dynasty-sports-report/dynasty-sports-report-logo.svg" alt="Dynasty Sports Report logo" title="DynastySportsReport.com website logo">
+					</div>
+					<img class="dmesa-home-img show-for-large" src="/img/projects/dmesa/dmesa-logo.svg" alt="d'Mesa Home logo" title="d'Mesa Home logo">
+				</div>
+				<div class="medium-push-4 medium-3 columns hide-for-large text-center dmesa-home-wrapper column-2">
+					<img src="/img/projects/dmesa/dmesa-logo.svg" alt="d'Mesa Home logo" title="d'Mesa Home logo">
+				</div>
+				<div class="medium-5 large-8 columns jardin-de-france-wrapper column-3">
+					<img srcset="{{ cdn }}/img/projects/jardin-de-france-tx/index/graphic-design/jardin-de-france-price-tag_2x.jpg 2x" src="{{ cdn }}/img/projects/jardin-de-france-tx/index/graphic-design/jardin-de-france-price-tag.jpg" alt="Jardin de France TX logo" title="Jardin de France TX product price tag design">
+				</div>
+			</div>
+		</div>
+	</div>
+</section>
