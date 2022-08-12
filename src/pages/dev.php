e786dbf64aa7107df899988c69a3e26823d3fa75
Nico Watine <nico@studioncreations.com>
2022-08-12 15:11:17 -0500

    Duplicate and push down for dev

--- src/pages/dev.php
+++ src/pages/dev.php
@@ -22,6 +22,20 @@ body-class: index bodyLayout-unique
 				</div>
 				<img src="https://studioncreations.com/wp-content/uploads/2022/01/iFixit-Toolkit-Review-Feature-Image-1024x409.jpg" alt="">
 			</a>
+
+			<br>
+			<br>
+			<br>
+			<br>
+			<br>
+
+			<a href="" class="article-unit-link">
+				<div class="prose-wrap">
+					<h5 class="ff-monospace article-category">Product Review</h5>
+					<h3 class="ff-sans-serif article-headline">Is the iFixit Pro Tech Toolkit worth it?</h3>
+				</div>
+				<img src="https://studioncreations.com/wp-content/uploads/2022/01/iFixit-Toolkit-Review-Feature-Image-1024x409.jpg" alt="">
+			</a>
 		</div>
 		<div class="medium-6 columns"></div>
 	</div>
