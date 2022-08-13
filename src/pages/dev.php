cc9c6d7a6c8627f109dfa3e2abd567c0f9b5a124
Nico Watine <nico@studioncreations.com>
2022-08-12 19:23:55 -0500

    Update accurate

--- src/pages/dev.php
+++ src/pages/dev.php
@@ -39,9 +39,9 @@ body-class: index bodyLayout-unique
 				<img src="https://studioncreations.com/wp-content/uploads/2022/06/4-Coding-Bootcamp-Career-Benefits-Feature-image-1024x409.jpg" alt="">
 			</a>
 			<a href="https://studioncreations.com/blog/" class="article-unit-link">
-				<div class="prose-wrap">
-					<h5 class="ff-monospace article-category">Product Review</h5>
-					<h3 class="ff-sans-serif article-headline">Is the iFixit Pro Tech Toolkit worth it?</h3>
+				<div class="prose-wrap" id="read-more">
+					<!-- <h5 class="ff-monospace article-category">Blog Roll</h5> -->
+					<h3 class="ff-sans-serif article-headline">More Articles &gt;</h3>
 				</div>
 				<img src="https://studioncreations.com/wp-content/uploads/2022/06/Apple-TV-Sleep-Timer-Featured-image-1024x409.jpg" alt="">
 			</a>
