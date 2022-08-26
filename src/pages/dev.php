78b594852dd86aa588cb27a740cd60969b3ce6e2
Nico Watine <nico@studioncreations.com>
2022-08-26 10:42:19 -0500

    Duplicate section for step-2

--- src/pages/dev.php
+++ src/pages/dev.php
@@ -9,7 +9,18 @@ body-id: index
 body-class: index bodyLayout-unique
 ---
 <section class="web-development-steps-1-2-3" style="padding-top: 60px;">
-	<div class="grid-x align-middle step-1">
+	<div class="grid-x align-middle step-1" style="margin-bottom: 60px;">
+		<div class="cell large-6">
+			<img src="https://via.placeholder.com/1920x1080" alt="">
+		</div>
+		<div class="cell large-6" style="padding-left: 30px;">
+			<div class="prose-wrap" style="max-width: 440px;">
+				<h2 class="h3 mb-16"><span class="h5 display-block">Step 1</span> Project Mapping</h2>
+				<p class="lead">First we establish your project goals and how to achieve them with a well-crafted content strategy.</p>
+			</div>
+		</div>
+	</div>
+	<div class="grid-x align-middle step-2">
 		<div class="cell large-6">
 			<img src="https://via.placeholder.com/1920x1080" alt="">
 		</div>
