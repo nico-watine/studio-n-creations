8872a3988c0750eaf2e8d6214fc2752cb58c6194
Nico Watine <nico@studioncreations.com>
2022-08-26 11:05:36 -0500

    Move inlines to scss

--- src/pages/dev.php
+++ src/pages/dev.php
@@ -13,7 +13,7 @@ body-class: index bodyLayout-unique
 		<div class="cell large-6">
 			<img src="https://via.placeholder.com/1920x1080" alt="">
 		</div>
-		<div class="cell large-6" style="padding-left: 30px;">
+		<div class="cell large-6">
 			<div class="prose-wrap">
 				<h2 class="h3 mb-16"><span class="h5 display-block">Step 1</span> Project Mapping</h2>
 				<p class="lead">First we establish your project goals and how to achieve them with a well-crafted content strategy.</p>
@@ -35,7 +35,7 @@ body-class: index bodyLayout-unique
 		<div class="cell large-6">
 			<img src="https://via.placeholder.com/1920x1080" alt="">
 		</div>
-		<div class="cell large-6" style="padding-left: 30px;">
+		<div class="cell large-6">
 			<div class="prose-wrap">
 				<h2 class="h3 mb-16"><span class="h5 display-block">Step 3</span> Development Stage</h2>
 				<p class="lead">With the content established and a solid user experience defined, the project gets developed into a functioning website. I utilize the industry’s latest technologies to ensure optimal performance on all device platforms and screen sizes.</p>
