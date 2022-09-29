64bdb2b437c7d0d02ef40a4497a9d83b6e569bf3
Nico Watine <nico@studioncreations.com>
2022-09-29 14:00:48 -0500

    Another height tweak

--- src/pages/dev.php
+++ src/pages/dev.php
@@ -10,10 +10,10 @@ body-class: index bodyLayout-unique
 ---
 <section class="web-development-steps-1-2-3-4" style="padding-top: 60px;">
 	<div class="grid-x align-middle step-wrap step-1">
-		<div class="cell large-6 show-for-medium">
+		<div class="cell medium-6 show-for-medium">
 			<img src="https://via.placeholder.com/1920x1080" alt="">
 		</div>
-		<div class="cell large-6">
+		<div class="cell medium-6">
 			<div class="prose-wrap">
 				<h2 class="h3 ff-monospace mb-16"><span class="h5 display-block">Step 1</span> Project Mapping</h2>
 				<img class="hide-for-medium" src="https://via.placeholder.com/1920x1080" alt="">
@@ -22,22 +22,22 @@ body-class: index bodyLayout-unique
 		</div>
 	</div>
 	<div class="grid-x align-middle step-wrap step-2">
-		<div class="cell large-6">
+		<div class="cell medium-6">
 			<div class="prose-wrap float-right">
 				<h2 class="h3 ff-monospace mb-16"><span class="h5 display-block">Step 2</span> Design Phase</h2>
 				<img class="hide-for-medium" src="https://via.placeholder.com/1920x1080" alt="">
 				<p class="lead">Next comes the design process, during which the site’s graphics are visualized and prose is written. After our close consultation, I'll provide mockups and a style guide.</p>
 			</div>
 		</div>
-		<div class="cell show-for-medium large-6">
+		<div class="cell show-for-medium medium-6">
 			<img src="https://via.placeholder.com/1920x1080" alt="">
 		</div>
 	</div>
 	<div class="grid-x align-middle step-wrap step-3">
-		<div class="cell show-for-medium large-6">
+		<div class="cell show-for-medium medium-6">
 			<img src="https://via.placeholder.com/1920x1080" alt="">
 		</div>
-		<div class="cell large-6">
+		<div class="cell medium-6">
 			<div class="prose-wrap">
 				<h2 class="h3 ff-monospace mb-16"><span class="h5 display-block">Step 3</span> Development Stage</h2>
 				<img class="hide-for-medium" src="https://via.placeholder.com/1920x1080" alt="">
