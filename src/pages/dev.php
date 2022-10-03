b4d614483af47e9befe6da0c6162b9f682617d86
Nico Watine <nico@studioncreations.com>
2022-10-03 11:02:10 -0500

    Copy splash markup from index

--- src/pages/dev.php
+++ src/pages/dev.php
@@ -8,3 +8,23 @@ og-image: /img/assets/og-images/index/studio-n-creations-og-image-1.png
 body-id: index
 body-class: index bodyLayout-unique
 ---
+<header class="splash-video-header grid-x position-relative overflow-hidden">
+	<div class="grid-container fluid width-100">
+		<div class="grid-x grid-padding-x align-middle prose-grid">
+			<div class="prose-cell cell color-white large-text-center">
+				<div class="grid-container">
+					<h1>Studio N Creations</h1>
+					<h2>Web Design |&nbsp;Site&nbsp;Maintenance</h2>
+				</div>
+			</div>
+		</div>
+	</div>
+	<div class="grid-x position-absolute backgroundVideo-container width-100 height-100">
+		<div class="cell">
+			<video playsinline autoplay muted loop preload="" poster="{{ cdn }}/img/index/website-mashup-video/studio-n-creations-website-mashup-poster.jpg" id="backgroundVideo">
+				<source src="{{ cdn }}/img/index/website-mashup-video/studio-n-creations-website-mashup.webm" type="video/webm">
+				<source src="{{ cdn }}/img/index/website-mashup-video/studio-n-creations-website-mashup.mp4" type="video/mp4">
+			</video>
+		</div>
+	</div>
+</header>
