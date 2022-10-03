611407d9157609d027f8df93f4bfcf2551775caf
Nico Watine <nico@studioncreations.com>
2022-10-03 11:13:52 -0500

    Add id to scroll section

--- src/pages/dev.php
+++ src/pages/dev.php
@@ -31,3 +31,32 @@ body-class: index bodyLayout-unique
 		</div>
 	</div>
 </header>
+
+<main class="index-webpage-collage-container" id="main-content-section" data-magellan-target="main-content-section">
+	<section class="intro-slogan">
+		<div class="row">
+			<div class="medium-6 columns medium-text-right">
+				<h2>I create phone-friendly websites for projects of all&nbsp;sizes</h2>
+			</div>
+			<div class="medium-5 columns end">
+				<div class="prose">
+					<p class="lead">No client is too big or too small. Whether you need a single brochure-type landing page or a full featured e-commerce site, I've got you covered.</p>
+				</div>
+			</div>
+		</div>
+	</section>
+	<section class="about-me">
+		<div class="row">
+			<div class="column">
+				<div class="prose-wrap float-center">
+					<div class="prose float-center" itemscope itemtype="http://schema.org/Person">
+						<h3 class="h3 ff-monospace">About Me</h3>
+						<h4 class="h4 mb-16">My name is <span itemprop="name">Nico Watine</span>, and I am the <span itemprop="affiliation">Studio&nbsp;N&nbsp;Creations</span> team. I design, code and maintain mobile-optimized websites for projects of all sizes.</h4>
+						<p>Having a professional website is key to a company’s image and success, and that’s exactly what I specialize in — strong websites for B2B and B2C small businesses. While I used to do full-stack web development full time, I now offer my services as a part-time <span itemprop="jobTitle">freelance web designer</span>. I only take on 1 or 2 projects at a time to ensure I can provide the utmost excellence in service and quality with every job, for every client.</p>
+						<p>I’m located in Houston, TX, and am available for hire anywhere. Please <a data-open="contactModal">contact me</a> and let’s see when we can get started — I’m only a message away.</p>
+					</div>
+				</div>
+			</div>
+		</div>
+	</section>
+</main>
