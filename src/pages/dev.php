2cf8a7554700813366a6c033a8dc488ec22d7f0b
Nico Watine <nico@studioncreations.com>
2022-10-19 15:31:18 -0500

    Prepare dev page

--- src/pages/dev.php
+++ src/pages/dev.php
@@ -8,30 +8,6 @@ og-image: /img/assets/og-images/index/studio-n-creations-og-image-1.png
 body-id: index
 body-class: index bodyLayout-unique
 ---
-<header class="splash-video-header grid-x position-relative overflow-hidden">
-	<div class="grid-container fluid width-100">
-		<div class="grid-x grid-padding-x align-middle prose-grid">
-			<div class="prose-cell cell color-white large-text-center">
-				<div class="grid-container">
-					<h1>Studio N Creations</h1>
-					<h2>Web Design |&nbsp;Site&nbsp;Maintenance</h2>
-					<div data-magellan data-threshold="0">
-						<a href="#main-content-section"><img src="/img/assets/icons/arrow-circle-down.svg" alt=""></a>
-					</div>
-				</div>
-			</div>
-		</div>
-	</div>
-	<div class="grid-x position-absolute backgroundVideo-container width-100 height-100">
-		<div class="cell">
-			<video playsinline autoplay muted loop preload="" poster="{{ cdn }}/img/index/website-mashup-video/studio-n-creations-website-mashup-poster.jpg" id="backgroundVideo">
-				<source src="{{ cdn }}/img/index/website-mashup-video/studio-n-creations-website-mashup.webm" type="video/webm">
-				<source src="{{ cdn }}/img/index/website-mashup-video/studio-n-creations-website-mashup.mp4" type="video/mp4">
-			</video>
-		</div>
-	</div>
-</header>
-
 <main class="index-webpage-collage-container" id="main-content-section" data-magellan-target="main-content-section">
 	<section class="intro-slogan">
 		<div class="row">
